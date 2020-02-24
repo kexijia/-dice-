8e8d8a1d8a638710c5b28e0a48f97485b3cf3e48
kexijia <44548601+kexijia@users.noreply.github.com>
2020-02-24 15:17:01 +0800

    first loss

---
+++ loss.py
@@ -0,0 +1,42 @@
+import torch
+import torchvision
+import torch.nn
+
+class VGG19(torch.nn.Module):
+    def __init__(self, requires_grad=False):
+        super().__init__()
+        
+        vgg_pretrained=torchvision.models.vgg19(prettrained=True).features
+        self.slice1= torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        self.slice5 = torh.nn.Sequential()
+        
+        for x in range(2):
+            self.slice1.add_module(str(x),vgg_pretrained_features[x])
+        for x in range(2, 7):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(7, 12):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(12, 21):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(21, 30):
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+            
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+                
+    
+    def forward(self, x):
+        h_relu1= self.slice1(x)
+        h_relu2 = self.slice2(h_relu1)
+        h_relu3 = self.slice3(h_relu2)
+        h_relu4 = self.slice4(h_relu3)
+        h_relu5 = self.slice5(h_relu4)
+        out = [h_relu1, h_relu2, h_relu3, h_relu4 h_relu5]
+        return out
+        
+        
+            
